8f32f5cf31ed6aa5c638edb900791752600cf3ae
hpc <philipchanhk626@gmail.com>
2020-06-18 13:37:12 -0400

    added gmaps, google types, search location adds marker to map

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import { GOOGLE_API } from './KEYS'
 import axios from 'axios'
 
+// dynamic loading of gmaps
+declare global {
+  interface Window {
+    initMap: Function;
+  }
+}
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API}&callback=initMap`;
+script.defer = true;
+script.async = true;
+
+window.initMap = function () {
+  // JS API is loaded and available
+};
+document.head.appendChild(script);
+
+
+
+
+
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
@@ -25,16 +46,21 @@ function searchAddressHandler(event: Event) {
       enteredAddress
     )}&key=${GOOGLE_API}`
   ).then(res => {
-    // console.log(res)
     if (res.data.status !== 'OK') {
       throw new Error('Could not fetch location')
     }
     const coordinates = res.data.results[0].geometry.location
     console.log(coordinates)
+    const map = new google.maps.Map(document.getElementById("map")!, {
+      center: coordinates,
+      zoom: 16
+    });
+
+    new google.maps.Marker({position: coordinates, map: map});
   }).catch(err => {
     alert(err.message)
     console.log(err)
   })
 }
 
-form?.addEventListener('submit', searchAddressHandler)!;
+form.addEventListener('submit', searchAddressHandler)!;
